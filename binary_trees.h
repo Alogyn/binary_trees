904d7476d767188f77c20942ec0c9a3cb9fdbb99
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-28 03:04:13 -0800

    Measure the size of a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -71,4 +71,7 @@ size_t binary_tree_height(const binary_tree_t *tree);
 /* Task 10. Depth */
 size_t binary_tree_depth(const binary_tree_t *tree);
 
+/* Task 11. Size */
+size_t binary_tree_size(const binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
