01d4c4e321fc0a1d4ae32b53064212d79276a97f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-28 01:52:12 -0800

    Check if a node is a leaf

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* Task 3. Delete */
 void binary_tree_delete(binary_tree_t *tree);
 
+/* Task 4. Is leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
