bc22d1d6859ad58dbc295847c6a86b2481bc709f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-02-28 01:56:35 -0800

    Check if a given node is a root

--- binary_trees.h
+++ binary_trees.h
@@ -53,4 +53,7 @@ void binary_tree_delete(binary_tree_t *tree);
 /* Task 4. Is leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* Task 5. Is root */
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
