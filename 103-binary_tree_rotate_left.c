08142e0bce6daa44864b3b58cf3d8a0688560f71
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-03-01 00:55:29 +0100

    Perform a left-rotation on a binary tree

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -12,20 +12,27 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root;
+	binary_tree_t *pivot, *tmp;
 
-	if (!tree || !tree->right)
-		return (tree);
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
 
-	new_root = tree->right;
-	tree->right = new_root->left;
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
 
-	if (new_root->left)
-		new_root->left->parent = tree;
-
-	new_root->left = tree;
-	new_root->parent = tree->parent;
-	tree->parent = new_root;
-
-	return (new_root);
+	return (pivot);
 }
