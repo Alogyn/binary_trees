ddd65b60e662859cf88b4b5a7661017abe893ac2
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-03-01 01:00:26 +0100

    Measure the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,15 +12,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((left_height > right_height) ? left_height : right_height);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
 
 /**
@@ -33,15 +33,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (tree == NULL)
-		return (0);
-
-	/* Calculate heights of left and right subtrees */
-	left_height = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
-
-	/* Balance factor is left height minus right height */
-	return (left_height - right_height);
+	return (0);
 }
